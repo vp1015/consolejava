a4ddba01cc0965a4430e7b25d1c68b0dfbc4e58e
vp96 <vinayakpatil209@gmail.com>
2022-08-05 18:30:08 +0530

    student updated

--- consolejava/src/consolejava/Student.java
+++ consolejava/src/consolejava/Student.java
@@ -5,7 +5,7 @@ public class Student {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Hello students");
-System.out.println("git session");
+System.out.println("git session today");
 	}
 
 }
